610bf75717e45e8e28604d0d0cab93dab78e3b7b
Ryan El Kochta <git@relkochta.com>
2025-05-05 19:39:05 -0400

    kmer: aaaaaaaaaaaaaaa

--- src/kmer.rs
+++ src/kmer.rs
@@ -4,39 +4,48 @@ use std::{
     collections::{VecDeque, vec_deque},
 };
 
-#[derive(Copy, Clone, Debug)]
-pub enum Kmer {
-    Data { start_pos: usize, length: usize },
+#[derive(Debug, Eq)]
+pub enum Kmer<'a> {
+    Data(&'a [u8]),
     Sentinel,
 }
 
 #[derive(Debug)]
-pub struct SuperKmer {
+pub struct SuperKmer<'a> {
     pub start_pos: usize,
     pub length: usize,
-    pub minimizer: Kmer,
+    pub minimizer: Kmer<'a>,
 }
 
-impl Kmer {
-    pub fn cmp(&self, other: &Self, representation: &[u8]) -> Ordering {
-        todo!()
+impl<'a, 'b> PartialEq<Kmer<'b>> for Kmer<'a> {
+    fn eq(&self, other: &Kmer<'b>) -> bool {
+        match (self, other) {
+            (&Kmer::Sentinel, &Kmer::Sentinel) => true,
+            (&Kmer::Data(d1), &Kmer::Data(d2)) => d1 == d2,
+            _ => false,
+        }
+    }
+}
+
+impl<'a, 'b> PartialOrd<Kmer<'b>> for Kmer<'a> {
+    fn partial_cmp(&self, other: &Kmer<'b>) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<'a> Ord for Kmer<'a> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (&Kmer::Sentinel, &Kmer::Sentinel) => Ordering::Equal,
+            (&Kmer::Data(d1), &Kmer::Data(d2)) => d1.cmp(d2),
+            (&Kmer::Sentinel, &Kmer::Data(_)) => Ordering::Greater,
+            (&Kmer::Data(_), &Kmer::Sentinel) => Ordering::Less,
+        }
     }
 }
 
 pub fn to_kmers(representation: &[u8], k: usize) -> Vec<Kmer> {
-    let mut kmers: Vec<_> = (0..representation.len())
-        .collect::<Vec<_>>()
-        .windows(k)
-        .map(|slice| Kmer::Data {
-            start_pos: slice[0],
-            length: slice.len(),
-        })
-        .collect();
-
-    // Ensure that we always have the sentinel character
-    kmers.push(Kmer::Sentinel);
-
-    kmers
+    todo!()
 }
 
 // Based on DP solution at https://algo.monster/liteproblems/239
