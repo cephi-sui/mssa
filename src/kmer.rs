fa9c8e6def595e99dec5a6c103921a49c7e70abd
Ryan El Kochta <git@relkochta.com>
2025-05-05 16:13:08 -0400

    kmer: finished kmer

--- src/kmer.rs
+++ src/kmer.rs
@@ -1,19 +1,23 @@
 use anyhow::Result;
 use std::cmp::{Ord, Ordering};
 
-#[derive(PartialEq, Eq, Debug)]
+#[derive(Debug, Eq)]
 pub struct Kmer<'a>(&'a [u8]);
 
+impl<'a, 'b> PartialEq<Kmer<'b>> for Kmer<'a> {
+    fn eq(&self, other: &Kmer<'b>) -> bool {
+        self.0.eq(other.0)
+    }
+}
+
 impl<'a, 'b> PartialOrd<Kmer<'b>> for Kmer<'a> {
-    fn partial_cmp(&'a self, other: &Self) -> Option<Ordering> {
+    fn partial_cmp(&self, other: &Kmer<'b>) -> Option<Ordering> {
         self.0.partial_cmp(other.0)
     }
 }
 
-/*
-impl Ord for Kmer {
+impl<'a> Ord for Kmer<'a> {
     fn cmp(&self, other: &Self) -> Ordering {
         self.0.cmp(&other.0)
     }
 }
-*/
